972ad9234fda8fc84080d0a98c91aa2ba644d6b3
Cristopher Vanegas <cristopher.platzi@gmail.com>
2024-06-18 08:43:15 -0500

    Added 3 projects done in  module 1 C# Programming Language P1. Advanced Calculator P2. Library Manager P3. Products Inventory

---
+++ 1_MODULE_CS/CalculadoraAvanzada/CalculadoraAvanzada/Program.cs
@@ -0,0 +1,248 @@
+﻿using System.Runtime.Intrinsics.Arm;
+
+namespace CalculadoraAvanzada
+{
+    public class Calculadora
+    {
+        // Método para sumar dos números
+        public double Sumar(double a, double b)
+        {
+            return a + b;
+        }
+
+        // Método para restar dos números
+        public double Restar(double a, double b)
+        {
+            return a - b;
+        }
+
+        // Método para multiplicar dos números
+        public double Multiplicar(double a, double b)
+        {
+            return a * b;
+        }
+
+        // Método para dividir dos números
+        public double Dividir(double a, double b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("No se puede dividir entre cero.");
+            }
+            return a / b;
+        }
+    }
+
+    internal class Program
+    {        
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            List<int> nums = new List<int>();
+            Calculadora calculadora = new Calculadora();
+
+            int opt = 0, aux = 0;
+            double a, b;
+
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Bienvenido a la calculadora avanzada!");
+                Console.WriteLine("[1] Suma");
+                Console.WriteLine("[2] Resta");
+                Console.WriteLine("[3] Multiplicacion");
+                Console.WriteLine("[4] Division");
+                Console.WriteLine("[5] Mayor");
+                Console.WriteLine("[6] Menor");
+                Console.WriteLine("[7] Salir\n");
+
+                try
+                {
+                    Console.Write("Elige una opcion: ");
+                    opt = Convert.ToInt16(Console.ReadLine());
+                    Console.ForegroundColor = ConsoleColor.Red;
+                }
+                catch (FormatException ex)
+                {
+                    // Manejar la excepción FormatException
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Error de formato: {ex.Message}");
+                    Console.WriteLine("Por favor, ingrese un número válido.");
+                }
+                
+                switch (opt)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("SUMA");
+                            Console.ResetColor();
+                            Console.Write("Ingrese el 1er numero: ");
+                            a = Convert.ToInt64(Console.ReadLine());
+                            Console.Write("Ingrese el 2do numero: ");
+                            b = Convert.ToInt64(Console.ReadLine());
+                            Console.Write($"La SUMA de {a} + {b} es ");
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            Console.Write($"{ calculadora.Sumar(a, b)}\n");
+                            Console.ResetColor();
+                        }
+                        break;
+
+                    case 2:
+                        {
+                            Console.WriteLine("RESTA");
+                            Console.ResetColor();
+                            Console.Write("Ingrese el 1er numero: ");
+                            a = Convert.ToInt64(Console.ReadLine());
+                            Console.Write("Ingrese el 2do numero: ");
+                            b = Convert.ToInt64(Console.ReadLine());
+                            Console.Write($"La RESTA de {a} - {b} es ");
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            Console.Write($"{calculadora.Restar(a, b)}\n");
+                            Console.ResetColor();
+                        }
+                        break;
+
+                    case 3:
+                        {
+                            Console.WriteLine("MULTIPLICACION");
+                            Console.ResetColor();
+                            Console.Write("Ingrese el 1er numero: ");
+                            a = Convert.ToInt16(Console.ReadLine());
+                            Console.Write("Ingrese el 2do numero: ");
+                            b = Convert.ToInt16(Console.ReadLine());
+                            Console.Write($"La MULTIPLICACION de {a} X {b} es ");
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            Console.Write($"{calculadora.Multiplicar(a, b)}\n");
+                            Console.ResetColor();
+                        }
+                        break;
+
+                    case 4:
+                        {
+                            Console.WriteLine("DIVISION");
+                            Console.ResetColor();
+                            Console.Write("Ingrese el 1er numero: ");
+                            a = Convert.ToInt16(Console.ReadLine());
+
+                            do
+                            {
+                                Console.Write("Ingrese el 2do numero (distinto de cero): ");
+                                b = Convert.ToInt16(Console.ReadLine());
+
+                                if (b == 0)
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Red;
+                                    Console.WriteLine("El segundo número no puede ser cero. Por favor, ingrese un valor distinto de cero.");
+                                    Console.ResetColor();
+                                }
+                            } while (b == 0);
+
+                            try
+                            {
+                                Console.Write($"La DIVISION de {a} / {b} es ");
+                                Console.ForegroundColor = ConsoleColor.Yellow;
+                                Console.Write($"{calculadora.Dividir(a, b)}\n");
+                                Console.ResetColor();
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine(ex.Message);
+                            }
+                        }
+                        break;
+
+                    case 5:
+                        {
+                            Console.WriteLine("NUMERO MAYOR DE UNA LISTA");
+                            Console.Write("Generando numeros aleatorios");
+                            for (int i = 0; i<3; i++)
+                            {
+                                Console.Write('.');
+                                Thread.Sleep(500);
+                            }
+                            // General los numeros aleatorios
+                            for (int i = 0; i < 10; i++)
+                                nums.Add(random.Next(1, 500));
+
+                            Console.ResetColor();
+                            // Muestra los numeros aleatorios
+                            Console.Write("\nLa lista de numeros aleatorios es: ");
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            for (int i = 0; i < 10; i++)
+                                Console.Write($"{nums[i]} ");
+
+                            // Muestra el mayor
+                            aux = nums[0];
+                            for (int i = 0;i < 10; i++)
+                            {
+                                if (aux <  nums[i]) { aux = nums[i]; }
+                            }
+                            Console.ResetColor();
+                            Console.Write("\nEl numero mayor es ");
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine($"{aux}");
+
+                            // Vacia la lista
+                            nums.Clear();
+                        } break;
+
+                    case 6:
+                        {
+                            Console.WriteLine("NUMERO MENOR DE UNA LISTA");
+                            Console.Write("Generando numeros aleatorios");
+                            for (int i = 0; i < 3; i++)
+                            {
+                                Console.Write('.');
+                                Thread.Sleep(500);
+                            }
+                            // General los numeros aleatorios
+                            for (int i = 0; i < 10; i++)
+                                nums.Add(random.Next(1, 500));
+
+                            Console.ResetColor();
+                            // Muestra los numeros aleatorios
+                            Console.Write("\nLa lista de numeros aleatorios es: ");
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            for (int i = 0; i < 10; i++)
+                                Console.Write($"{nums[i]} ");
+
+                            // Muestra el mayor
+                            aux = nums[0];
+                            for (int i = 0; i < 10; i++)
+                            {
+                                if (aux > nums[i]) { aux = nums[i]; }
+                            }
+                            Console.ResetColor();
+                            Console.Write("\nEl numero menor es ");
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine($"{aux}");
+
+                            // Vacia la lista
+                            nums.Clear();
+                        }
+                        break;
+
+                    case 7:
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("\nByeeee!");
+                            Console.WriteLine("Programa hecho por Cristopher Vanegas...\n\n");
+                            Console.ResetColor();
+                            Environment.Exit(0);
+                        }
+                        break;
+
+
+                    default:
+                        {
+                            Console.WriteLine("Ingrese una opcion valida!");
+                        }
+                        break;
+                }
+                Console.WriteLine("\nPresione la tecla enter...");
+                Console.ReadLine();
+            } while (opt != 7);
+        }
+    }
+}
---
+++ 1_MODULE_CS/INVENTARIO_DE_PRODUCTOS/INVENTARIO_DE_PRODUCTOS/Program.cs
@@ -0,0 +1,434 @@
+﻿/*
+ INTEGRANTES
+JOSHUA BRYAN SAREZ CHIRIGUAYA
+BAÑO CORDERO CHRISTELL NICOLE
+BORIS ISMAEL SUAREZ CONTRERAS
+CRISTOPHER JULINHO VANEGAS SANTI
+ */
+
+using System.Runtime.Intrinsics.X86;
+using System;
+using System.Runtime.Intrinsics.Arm;
+
+namespace INVENTARIO_DE_PRODUCTOS
+{
+    class Producto
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public double precio { get; set; }
+        public int stock { get; set; }
+    }
+    class Inventario
+    {
+        public List<Producto> listaProductos = new List<Producto>();
+        string nombreProducto = "";
+        double precioProducto = 0.00;
+        int stockProducto = 0;
+
+        public void IngresarProducto()
+        {
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("AGREGAR PRODUCTO");
+                Console.ResetColor();
+                Console.Write("Ingrese el nombre del producto: ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                nombreProducto = Console.ReadLine();
+                Console.ResetColor();
+
+                Console.Write("Ingrese el precio del producto: ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                precioProducto = double.Parse(Console.ReadLine());
+                Console.ResetColor();
+
+                Console.Write("Ingrese el stock del producto: ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                stockProducto = int.Parse(Console.ReadLine());
+                Console.ResetColor();
+
+                if (String.IsNullOrWhiteSpace(nombreProducto))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Ingrese un nombre de producto no vacio");
+                }
+            }
+            while (String.IsNullOrWhiteSpace(nombreProducto));
+
+
+            listaProductos.Add(new Producto
+            {
+                id = listaProductos.Count + 1,
+                nombre = nombreProducto,
+                precio = precioProducto,
+                stock = stockProducto
+            });
+        }
+
+        public void InicializarInventario()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("INICIALIZANDO INVENTARIO");
+            listaProductos = new List<Producto>() {
+                new Producto {
+                    id = 1,
+                    nombre = "Billetera",
+                    precio = 3.5,
+                    stock = 20
+                },
+                new Producto {
+                    id = 2,
+                    nombre = "Cartera",
+                    precio = 5.5,
+                    stock = 10
+                },
+                new Producto {
+                    id = 3,
+                    nombre = "Bolso",
+                    precio = 10.5,
+                    stock = 5
+                },
+                new Producto {
+                    id = 4,
+                    nombre = "Mochila",
+                    precio = 15.5,
+                    stock = 3
+                },
+            };
+        }
+
+        public void EliminarProducto()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("ELIMINAR PRODUCTO");
+            Console.Clear();
+            Console.Write("Ingrese el ID del producto a eliminar: ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            int idProducto = int.Parse(Console.ReadLine());
+
+            Producto productoAEliminar = listaProductos.Find(p => p.id == idProducto);
+            if (productoAEliminar != null)
+            {
+                listaProductos.Remove(productoAEliminar);
+                Console.WriteLine($"Producto con ID {idProducto} eliminado correctamente.");
+            }
+            else
+            {
+                Console.WriteLine($"Producto con ID {idProducto} no encontrado.");
+            }
+            Console.ResetColor();
+        }
+
+        public void MostrarProducto()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("MOSTRAR PRODUCTOS");
+            Console.ResetColor();
+            foreach (Producto producto in listaProductos)
+            {
+                Console.WriteLine($"ID del producto: {producto.id}, Nombre del producto: {producto.nombre}, Precio del producto: {producto.precio}, Stock: {producto.stock}");
+            }
+        }
+        public void BuscarPorNombre()
+        {
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("BUSCAR PRODUCTO POR NOMBRE");
+                Console.Clear();
+                Console.Write("Ingrese el nombre del producto a buscar: ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                nombreProducto = Console.ReadLine();
+                Console.ResetColor();
+
+                if (String.IsNullOrWhiteSpace(nombreProducto))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Ingrese un nombre de producto no vacio");
+                }
+            }
+            while (String.IsNullOrWhiteSpace(nombreProducto));
+
+
+            foreach (Producto producto in listaProductos)
+            {
+                if (producto.nombre == nombreProducto)
+                    Console.WriteLine($"ID del producto: {producto.id}, Nombre del producto: {producto.nombre}, Precio del producto: {producto.precio}, Stock: {producto.stock}");
+            }
+        }
+        public void ActualizarProducto()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("ACTUALIZAR PRODUCTO");
+            Console.ResetColor();
+
+            int idActualizar = 0;
+            string nuevoNombre = "";
+            double nuevoPrecio = 0.00;
+            int nuevoStock = 0;
+            bool isValidEntry = false;
+
+
+            // PREGUNTO por el ID del producto que desea actualizar
+            do
+            {
+                // TRY CATCH para que el ID sea un valor esperado
+                try
+                {
+                    Console.ResetColor();
+                    Console.Write("Ingrese el ID del producto que desea actualizar: ");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    idActualizar = Convert.ToInt16(Console.ReadLine());
+
+                    if (idActualizar >= 0)
+                        isValidEntry = true;
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Ingrese un ID mayor o igual a 0!");
+                    }
+                }
+                catch (FormatException ex)
+                {
+                    // Manejar la excepción FormatException
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Error de formato: {ex.Message}");
+                    Console.WriteLine("Por favor, ingrese solo enteros.");
+                }
+            }
+            while (!isValidEntry);
+
+            // PREGUNTO por el nuevo NOMBRE
+            isValidEntry = false;
+            do
+            {
+                // Actualizar NOMBRE
+                Console.ResetColor();
+                Console.Write("El nombre anterior del producto es ");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write($"{listaProductos[idActualizar].nombre}\n");
+                Console.ResetColor();
+                Console.Write($"Ingrese el nuevo nombre para el producto: ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                nuevoNombre = Console.ReadLine();
+
+                if (!String.IsNullOrWhiteSpace(nuevoNombre))
+                {
+                    listaProductos[idActualizar].nombre = nuevoNombre;  // guardo el nuevo nombre en la lista
+                    isValidEntry = true;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Ingrese un nombre que no este vacio!");
+                }
+            }
+            while (!isValidEntry);
+
+            // PREGUNTO por el nuevo PRECIO
+            isValidEntry = false;
+            do
+            {
+                // TRY CATCH si desea actualizar PRECIO
+                try
+                {
+                    Console.ResetColor();
+                    Console.Write("El precio anterior del producto es ");
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write($"{listaProductos[idActualizar].precio}\n");
+                    Console.ResetColor();
+                    Console.Write($"Ingrese el nuevo precio para el producto: ");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    nuevoPrecio = double.Parse(Console.ReadLine());
+                }
+                catch (FormatException ex)
+                {
+                    // Manejar la excepción FormatException
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Error de formato: {ex.Message}");
+                    Console.WriteLine("Por favor, ingrese un número válido.");
+                }
+
+                if (nuevoPrecio > 0)
+                {
+                    listaProductos[idActualizar].precio = nuevoPrecio;  // guardo el nuevo nombre en la lista
+                    isValidEntry = true;
+                }
+                else {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Ingrese un valor mayor a 0!");
+                }
+            }
+            while (!isValidEntry);
+
+            // PREGUNTO por el nuevo STOCK
+            isValidEntry = false;
+            do
+            {
+                // TRY CATCH si desea actualizar STOCK
+                try
+                {
+                    Console.Write("El STOCK anterior del producto es ");
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write($"{listaProductos[idActualizar].precio}\n");
+                    Console.ResetColor();
+                    Console.Write($"Ingrese el nuevo STOCK para el producto: ");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    nuevoStock = int.Parse(Console.ReadLine());
+                }
+                catch (FormatException ex)
+                {
+                    // Manejar la excepción FormatException
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Error de formato: {ex.Message}");
+                    Console.WriteLine("Por favor, ingrese un número válido.");
+                }
+
+                if (nuevoStock > 0)
+                {
+                    listaProductos[idActualizar].precio = nuevoPrecio;  // guardo el nuevo nombre en la lista
+                    isValidEntry = true;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Ingrese un valor mayor a 0!");
+                }
+            }
+            while (!isValidEntry);
+        }
+        public void CalcularValorTotalInventario()
+        {
+            double valorTotal = 0;
+            foreach (var producto in listaProductos)
+            {
+                valorTotal += producto.precio * producto.stock;
+            }
+            Console.WriteLine($"El valor total del inventario es: {valorTotal:C}");
+        }
+        public void CalcularCantidadTotalProductos()
+        {
+            int cantidadTotal = 0;
+            foreach (var producto in listaProductos)
+            {
+                cantidadTotal += producto.stock;
+            }
+            Console.WriteLine($"La cantidad total de productos en el inventario es: {cantidadTotal}");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Inventario inventario = new Inventario();
+            inventario.InicializarInventario();     // Llena el inventario de productos.
+
+            int opt = 0;
+            do
+            {
+                Console.Clear();    // Limpia la pantalla al inicio
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("BIENVENIDOS AL INVENTARIO DE PRODUCTOS");
+                Console.ResetColor();
+                Console.WriteLine("[1] Agregar      Producto");
+                Console.WriteLine("[2] Eliminar     Producto");
+                Console.WriteLine("[3] Actualizar   Producto");
+                Console.WriteLine("[4] Mostrar      Productos");
+                Console.WriteLine("[5] Buscar       Producto por Nombre");
+                Console.WriteLine("[6] Calcular     estadistica del Inventario");
+                Console.WriteLine("[7] Salir\n");
+
+                try
+                {
+                    Console.Write("Elige una opcion: ");
+                    opt = Convert.ToInt16(Console.ReadLine());
+                    Console.ForegroundColor = ConsoleColor.Red;
+                }
+                catch (FormatException ex)
+                {
+                    // Manejar la excepción FormatException
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Error de formato: {ex.Message}");
+                    Console.WriteLine("Por favor, ingrese un número válido.");
+                }
+
+                Console.Clear();
+                switch (opt)
+                {
+                    case 1:
+                        {
+                            inventario.IngresarProducto();
+                        }
+                        break;
+
+                    case 2:
+                        {
+                            inventario.EliminarProducto();
+                        }
+                        break;
+
+                    case 3:
+                        {
+                            inventario.ActualizarProducto();
+                        }
+                        break;
+
+                    case 4:
+                        {
+                            inventario.MostrarProducto();
+                        }
+                        break;
+
+                    case 5:
+                        {
+                            inventario.BuscarPorNombre();
+                        }
+                        break;
+
+                    case 6:
+                        {
+                            Console.WriteLine("CALCULAR ESTADISTICA DEL INVENTARIO");
+                            inventario.MostrarProducto();
+                            inventario.CalcularValorTotalInventario();
+                            inventario.CalcularCantidadTotalProductos();
+                        }
+                        break;
+
+                    case 7:
+                        {
+                            List<String> integrantes = new List<String>();
+                            integrantes.Add("JOSHUA BRYAN SAREZ CHIRIGUAYA");
+                            integrantes.Add("BAÑO CORDERO CHRISTELL NICOLE");
+                            integrantes.Add("BORIS ISMAEL SUAREZ CONTRERAS");
+                            integrantes.Add("CRISTOPHER JULINHO VANEGAS SANTI");
+
+                            Console.WriteLine("\nByeeee!");
+                            Console.WriteLine("Programa hecho por: \n\n");
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            for (int i = 0; i < integrantes.Count; i++)
+                            {
+                                Thread.Sleep(500);
+                                Console.WriteLine(integrantes[i]);
+                            }
+
+                            Console.ResetColor();
+                            Environment.Exit(0);
+                        }
+                        break;
+
+
+                    default:
+                        {
+                            Console.WriteLine("Ingrese una opcion valida!");
+                        }
+                        break;
+                }
+                Console.WriteLine("\nPresione la tecla 'enter' para continuar...");
+                Console.ReadLine();
+            } while (opt != 7);
+        }
+    }
+
+}
