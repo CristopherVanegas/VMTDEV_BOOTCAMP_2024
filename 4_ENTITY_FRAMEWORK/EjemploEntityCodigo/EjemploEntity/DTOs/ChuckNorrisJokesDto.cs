6f7151abd0b1e8fe1f5a0cf026808a0cdd68abb6
Cristopher Vanegas <cristopher.platzi@gmail.com>
2024-07-13 13:51:34 -0500

    ADDED CHUCK NORRIS JOKED DTO TO PRESENT THE DATA DESERIALIZED IN THE 	CLASS.

---
+++ 4_ENTITY_FRAMEWORK/EjemploEntityCodigo/EjemploEntity/DTOs/ChuckNorrisJokesDto.cs
@@ -0,0 +1,16 @@
+﻿namespace EjemploEntity.DTOs
+{
+    // ChuckNorrisJokesDto myDeserializedClass = JsonConvert.DeserializeObject<ChuckNorrisJokesDto>(myJsonResponse);
+    public class ChuckNorrisJokesDto
+    {
+        public List<object> categories { get; set; }
+        public string created_at { get; set; }
+        public string icon_url { get; set; }
+        public string id { get; set; }
+        public string updated_at { get; set; }
+        public string url { get; set; }
+        public string value { get; set; }
+    }
+
+
+}
