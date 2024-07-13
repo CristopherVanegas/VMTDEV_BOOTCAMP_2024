5c22ea784f459e8367134a67b41ed54d598eb98c
Cristopher Vanegas <cristopher.platzi@gmail.com>
2024-07-13 13:52:29 -0500

    ADDED CHUCK NORRIS API CLASS IN THE UTILITARY DIRECTORY This class keep method to get data from the api and deserialize it using a DTO class, then returns "respuesta" to the controller in order to return an answer to client. Each method takes a url parameter. If an exception is find, it's handled by ControlError.LogErrorMetodos.

---
+++ 4_ENTITY_FRAMEWORK/EjemploEntityCodigo/EjemploEntity/Utilitarios/ChuckNorrisApi.cs
@@ -0,0 +1,56 @@
+﻿using EjemploEntity.DTOs;
+using EjemploEntity.Models;
+using Newtonsoft.Json;
+
+namespace EjemploEntity.Utilitarios
+{
+    public class ChuckNorrisApi
+    {
+        private ControlError log = new ControlError();
+        public async Task<Respuesta> GetChuckNorrisCategoriesApi(string url)
+        {
+            var respuesta = new Respuesta();
+            try
+            {
+                var client = new HttpClient();
+                var request = new HttpRequestMessage(HttpMethod.Get, url);
+                var response = await client.SendAsync(request);
+                response.EnsureSuccessStatusCode();
+
+                var json = await response.Content.ReadAsStringAsync();
+
+                respuesta.Cod = "000";
+                respuesta.Data = JsonConvert.DeserializeObject<List<string>>(json);
+                respuesta.Mensaje = "Se consumio correcto";
+            }
+            catch (Exception ex)
+            {
+                log.LogErrorMetodos("ChuckNorrisApi", "GetChuckNorrisCategoriesApi", ex.Message);
+            }
+            return respuesta;
+        }
+
+        public async Task<Respuesta> GetChuckNorrisJokesApi(string url)
+        {
+            var respuesta = new Respuesta();
+            try
+            {
+                var client = new HttpClient();
+                var request = new HttpRequestMessage(HttpMethod.Get, url);
+                var response = await client.SendAsync(request);
+                response.EnsureSuccessStatusCode();
+
+                var json = await response.Content.ReadAsStringAsync();
+
+                respuesta.Cod = "000";
+                respuesta.Data = JsonConvert.DeserializeObject<ChuckNorrisJokesDto>(json);
+                respuesta.Mensaje = "Se consumio correcto";
+            }
+            catch (Exception ex)
+            {
+                log.LogErrorMetodos("ChuckNorrisApi", "GetChuckNorrisJokesApi", ex.Message);
+            }
+            return respuesta;
+        }
+    }
+}
