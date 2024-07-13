7f58f7d4be1dd6c43fd4f0eb58f6efd5d5b4ccd6
Cristopher Vanegas <cristopher.platzi@gmail.com>
2024-07-13 11:20:43 -0500

    ADDED EXTRAS CONTROLLER TO HANDLE THE POKEAPI UTILITY TO CONSUME POKE API.

---
+++ 4_ENTITY_FRAMEWORK/EjemploEntityCodigo/EjemploEntity/Utilitarios/PokeApi.cs
@@ -0,0 +1,29 @@
+﻿using EjemploEntity.Models;
+
+namespace EjemploEntity.Utilitarios
+{
+    public class PokeApi
+    {
+		private ControlError log = new ControlError();
+        public async Task<Respuesta> GetPokeApi(string url)
+        {
+			var respuesta = new Respuesta();
+			try
+			{
+                var client = new HttpClient();
+                var request = new HttpRequestMessage(HttpMethod.Get, url);
+                var response = await client.SendAsync(request);
+                response.EnsureSuccessStatusCode();
+
+                respuesta.Cod = "000";
+                respuesta.Data = await response.Content.ReadAsStringAsync();
+                respuesta.Mensaje = "Se consumio correcto";
+            }
+            catch (Exception ex)
+			{
+				log.LogErrorMetodos("PokeApi", "GetPokeApi", ex.Message);
+			}
+			return respuesta;
+        }
+    }
+}
