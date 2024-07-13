1c3e298b379afd0dcd9ff233c1b5081009cfb4f8
Cristopher Vanegas <cristopher.platzi@gmail.com>
2024-07-13 13:44:58 -0500

    THERE WERE ADDED TWO NEW METHODS TO GET CHUCK NORRIS CATEGORIES AND CHUCK NORRIS JOKES GETS THE URL FROM APPSETTINGS -> KEYS THEN CALLS THE METHOD FROM THE CLASS ChuckNorrisApi IN THE UTILITARIOS DIRECTORY

--- 4_ENTITY_FRAMEWORK/EjemploEntityCodigo/EjemploEntity/Controllers/ExtrasController.cs
+++ 4_ENTITY_FRAMEWORK/EjemploEntityCodigo/EjemploEntity/Controllers/ExtrasController.cs
@@ -10,6 +10,7 @@ namespace EjemploEntity.Controllers
     {
         private ControlError log = new ControlError();
         private PokeApi pokeApi = new PokeApi();
+        private ChuckNorrisApi chuckNorrisApi = new ChuckNorrisApi();
         private readonly IConfiguration _configuration;
 
         public ExtrasController(IConfiguration configuration)
@@ -31,6 +32,45 @@ namespace EjemploEntity.Controllers
             catch (Exception ex)
             {
                 log.LogErrorMetodos("ExtrasController", "GetPokeApi", ex.Message);
+            }
+
+            return respuesta;
+        }
+
+        [HttpGet]
+        [Route("ChuckNorrisCategories")]
+        public async Task<Respuesta> ChuckNorrisCategories()
+        {
+            var respuesta = new Respuesta();
+            try
+            {
+                var url = _configuration.GetSection("Keys:ChuckNorrisCategoriesApi").Value!;
+
+                respuesta = await chuckNorrisApi.GetChuckNorrisCategoriesApi(url);
+            }
+            catch (Exception ex)
+            {
+                log.LogErrorMetodos("ExtrasController", "ChuckNorrisCategories", ex.Message);
+            }
+
+            return respuesta;
+        }
+
+        
+        [HttpGet]
+        [Route("GetChuckNorrisJokes")]
+        public async Task<Respuesta> GetChuckNorrisJokes()
+        {
+            var respuesta = new Respuesta();
+            try
+            {
+                var url = _configuration.GetSection("Keys:ChuckNorrisJokesApi").Value!;
+
+                respuesta = await chuckNorrisApi.GetChuckNorrisJokesApi(url);
+            }
+            catch (Exception ex)
+            {
+                log.LogErrorMetodos("ExtrasController", "GetChuckNorrisJokes", ex.Message);
             }
 
             return respuesta;
