5bc918654f02fd73421fea9aec2442327c449a23
Cristopher Vanegas <cristopher.platzi@gmail.com>
2024-07-15 08:42:11 -0500

    RENAMED VARIABLES AND METHODS TO SHORTTER VERSION. CREATED CnJokesByCateg TO CONSUME A RANDOM JOKE BY CATEGORY.

--- 4_ENTITY_FRAMEWORK/EjemploEntityCodigo/EjemploEntity/Utilitarios/ChuckNorrisApi.cs
+++ 4_ENTITY_FRAMEWORK/EjemploEntityCodigo/EjemploEntity/Utilitarios/ChuckNorrisApi.cs
@@ -6,8 +6,8 @@ namespace EjemploEntity.Utilitarios
 {
     public class ChuckNorrisApi
     {
-        private ControlError log = new ControlError();
-        public async Task<Respuesta> GetChuckNorrisCategoriesApi(string url)
+        private ControlError log = new();
+        public async Task<Respuesta> CnCategories(string url)
         {
             var respuesta = new Respuesta();
             try
@@ -25,12 +25,12 @@ namespace EjemploEntity.Utilitarios
             }
             catch (Exception ex)
             {
-                log.LogErrorMetodos("ChuckNorrisApi", "GetChuckNorrisCategoriesApi", ex.Message);
+                log.LogErrorMetodos("ChuckNorrisApi", "CnCategories", ex.Message);
             }
             return respuesta;
         }
 
-        public async Task<Respuesta> GetChuckNorrisJokesApi(string url)
+        public async Task<Respuesta> CnJokes(string url)
         {
             var respuesta = new Respuesta();
             try
@@ -48,7 +48,30 @@ namespace EjemploEntity.Utilitarios
             }
             catch (Exception ex)
             {
-                log.LogErrorMetodos("ChuckNorrisApi", "GetChuckNorrisJokesApi", ex.Message);
+                log.LogErrorMetodos("ChuckNorrisApi", "CnJokes", ex.Message);
+            }
+            return respuesta;
+        }
+
+        public async Task<Respuesta> CnJokesByCateg(string url, string category)
+        {
+            var respuesta = new Respuesta();
+            try
+            {
+                var client = new HttpClient();
+                var request = new HttpRequestMessage(HttpMethod.Get, $"{url}?category={category}");
+                var response = await client.SendAsync(request);
+                response.EnsureSuccessStatusCode();
+
+                var json = await response.Content.ReadAsStringAsync();
+
+                respuesta.Cod = "000";
+                respuesta.Data = JsonConvert.DeserializeObject<ChuckNorrisJokesDto>(json);
+                respuesta.Mensaje = "Se consumio correcto";
+            }
+            catch (Exception ex)
+            {
+                log.LogErrorMetodos("ChuckNorrisApi", "CnJokesByCateg", ex.Message);
             }
             return respuesta;
         }
